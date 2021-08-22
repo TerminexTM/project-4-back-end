5c90199f9cbc86360467828a74b05b5a167df56b
dooster <dustin2burns@yahoo.com>
2021-08-22 11:59:27 -0500

    fixed hash bug for users

--- products_api/urls.py
+++ products_api/urls.py
@@ -19,9 +19,9 @@ urlpatterns = [
 
 # User PATHS AND LOGIN
 
-    path('api/users', views.UserList.as_view(),name='user_list'),
+    path('api/users', views.UserAccountList.as_view(),name='user_list'),
 
-    path('api/users/<int:pk>', views.UserDetail.as_view(),name='user_detail'),
+    path('api/users/<int:pk>', views.UserAccountDetail.as_view(),name='user_detail'),
 
     path('api/users/login', csrf_exempt(views.check_user_login), name="check_user_login")
 
